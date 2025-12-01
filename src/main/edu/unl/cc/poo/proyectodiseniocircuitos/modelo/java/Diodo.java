389fd6acf226f7e3610dcea0bed4a76ad31909d5
Matias <matias.encarnacion@unl.edu.ec>
2025-11-30 21:31:58 -0500

    fix: Arreglo de problemas de inicializacion del objeto

--- src/main/edu/unl/cc/poo/proyectodiseniocircuitos/modelo/java/Diodo.java
+++ src/main/edu/unl/cc/poo/proyectodiseniocircuitos/modelo/java/Diodo.java
@@ -2,7 +2,15 @@
  * @Autor Grupo3
  */
 public class Diodo extends Componente{
+    public Diodo() {
+    }
+
     public Diodo(float voltaje, float corriente, int resistencia, float potencia, TipoMedida medida) {
         super(voltaje, corriente, resistencia, potencia, medida);
+    }
+
+    @Override
+    public String toString() {
+        return "Diodo{ Voltaje que pasa por el diodo: "+super.getVoltaje()+"}";
     }
 }
