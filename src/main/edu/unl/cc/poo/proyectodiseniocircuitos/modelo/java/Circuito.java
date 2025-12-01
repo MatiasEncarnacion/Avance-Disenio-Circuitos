5f88560bc5954a13ca4c908d4a1c78a67aac121b
Matias <matias.encarnacion@unl.edu.ec>
2025-11-30 21:28:17 -0500

    feat: Agregados metodos para obtener componente en una posicion determinada y obtener lenght del circuito

--- src/main/edu/unl/cc/poo/proyectodiseniocircuitos/modelo/java/Circuito.java
+++ src/main/edu/unl/cc/poo/proyectodiseniocircuitos/modelo/java/Circuito.java
@@ -9,26 +9,51 @@ public class Circuito {
     public Circuito() {
     }
 
-    public void agregar(Componente componente){
-        if(this.componentes == null){
+    public void agregar(Componente componente) {
+        if (this.componentes == null) {
             this.componentes = new ArrayList<>();
         }
-        if(!this.componentes.contains(componente)){
+        if (!this.componentes.contains(componente)) {
             this.componentes.add(componente);
         }
     }
 
-    public void agregar(Componente... componentes){
-        for(Componente componente: componentes){
+    public void agregar(Componente... componentes) {
+        for (Componente componente : componentes) {
             agregar(componente);
         }
     }
 
-    public void remover(Componente componente){
-        if(this.componentes.contains(componente)){
+    public void remover(Componente componente) {
+        if (this.componentes.contains(componente)) {
             this.componentes.remove(componente);
         }
     }
 
+    public int lenght() {
+        return this.componentes.size();
+    }
 
+    public Componente getComponente(int posicion) {
+        return componentes.get(posicion);
+    }
+
+    public Componente setComponente(int posicion, Componente componente) {
+        return componentes.set(posicion, componente);
+    }
+
+    public List<Componente> getComponentes() {
+        return componentes;
+    }
+
+    public void setComponentes(List<Componente> componentes) {
+        this.componentes = componentes;
+    }
+
+    @Override
+    public String toString() {
+        return "Circuito{" +
+                "componentes=" + componentes +
+                '}';
+    }
 }
