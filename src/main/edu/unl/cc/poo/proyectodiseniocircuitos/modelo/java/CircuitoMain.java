844da4c54d61309072750e4ce6a7559574aaed7e
Matias <matias.encarnacion@unl.edu.ec>
2025-11-30 21:29:27 -0500

    fix: Arreglado errores de excepciones y modificacion de circuitos

--- src/main/edu/unl/cc/poo/proyectodiseniocircuitos/modelo/java/CircuitoMain.java
+++ src/main/edu/unl/cc/poo/proyectodiseniocircuitos/modelo/java/CircuitoMain.java
@@ -5,54 +5,83 @@ import java.util.Scanner;
 /**
  * @Autor Grupo3
  */
+
 public class CircuitoMain {
+    public int identificar(Componente componente){
+        if(componente.getClass().getName().equals("Resistencia")){
+            return 1;
+        } else if(componente.getClass().getName().equals("Capacitor")){
+            return 2;
+        } else if(componente.getClass().getName().equals("Switch")){
+            return 3;
+        } else if(componente.getClass().getName().equals("Fuente")){
+            return 4;
+        } else if(componente.getClass().getName().equals("Diodo")){
+            return 5;
+        }  else if(componente.getClass().getName().equals("Transistor")){
+            return 6;
+        }
+        return 0;
+    }
     public static void main(String[] args) throws Exception {
+        CircuitoMain c = new CircuitoMain();
         Scanner sc = new Scanner(System.in);
         System.out.println("Bienvenido al Simulador de Circuitos");
         System.out.println("Seleccione el valor de la fuente de voltaje");
-        Componente fuente = new Fuente();
+        Fuente fuente = new Fuente();
         fuente.setVoltaje(sc.nextFloat());
-        List<Componente> componentes = new ArrayList<>();
-        componentes.add(fuente);
-        System.out.println("Lista de componentes: "+componentes);
+        Circuito circuito = new Circuito();
+        circuito.agregar(fuente);
+        System.out.println("Lista de componentes: "+circuito);
         int opcion;
         int opcion2;
         int posicion;
         float nuevovalor;
-        while(true){
+        boolean estado = true;
+        while(estado == true){
             System.out.println("1. Añadir componente");
             System.out.println("2. Modificar valores de componente");
+            System.out.println("3. Salir");
             opcion = sc.nextInt();
             switch(opcion){
                 case 1:
                     System.out.println("1. Resistencia");
                     System.out.println("2. Capacitor");
                     System.out.println("3. Switch");
+                    System.out.println("4. Diodo");
+                    System.out.println("5. Transistor");
                     opcion2 = sc.nextInt();
                     switch(opcion2){
                         case 1:
                             Resistencia resistencia = new Resistencia();
                             System.out.println("Ingrese el valor de resistencia");
-                            resistencia.setResistencia(sc.nextInt());
-                            resistencia.setVoltaje(fuente.getVoltaje());
-                            resistencia.calcularCorriente(resistencia.getVoltaje(), resistencia.getResistencia());
-                            resistencia.calcularPotencia((double)resistencia.getVoltaje(), resistencia.getResistencia());
+                            resistencia.setValores(fuente.getVoltaje(), sc.nextInt());
                             System.out.println("Resistencia: "+resistencia);
-                            componentes.add(resistencia);
+                            circuito.agregar(resistencia);
                             break;
                         case 2:
                             Capacitor capacitor = new Capacitor();
                             System.out.println("Ingrese el valor de capacitancia");
-                            capacitor.setCapacitancia(sc.nextFloat());
-                            capacitor.setVoltaje(fuente.getVoltaje());
-                            capacitor.calcularCargaElectrica(capacitor.getCapacitancia(), capacitor.getVoltaje());
-                            componentes.add(capacitor);
+                            capacitor.setValores(fuente.getVoltaje(), sc.nextFloat());
+                            circuito.agregar(capacitor);
                             break;
                         case 3:
                             Switch switchh = new Switch();
                             System.out.println("Encendido o apagado? (true o false)");
                             switchh.setEstado(sc.nextBoolean());
-                            componentes.add(switchh);
+                            circuito.agregar(switchh);
+                            break;
+                        case 4:
+                            Diodo diodo = new Diodo();
+                            System.out.println("Diodo creado");
+                            diodo.setVoltaje(fuente.getVoltaje());
+                            circuito.agregar(diodo);
+                            break;
+                        case 5:
+                            Transistor transistor = new Transistor();
+                            System.out.println("Ingrese el valor de frecuencia");
+                            transistor.setFrecuencia(sc.nextFloat());
+                            circuito.agregar(transistor);
                             break;
                     }
                     break;
@@ -60,32 +89,50 @@ public class CircuitoMain {
                         System.out.println("Ingrese la posicion del elemento a modificar");
                         posicion = sc.nextInt();
                         System.out.println("Componente seleccionado: ");
-                        componentes.get(posicion).toString();
-                        System.out.println("Que valor desea modificar?");
-                        System.out.println("1. Voltaje");
-                        System.out.println("2. Resistencia");
-                        System.out.println("3. Corriente");
-                        opcion2 = sc.nextInt();
+                        System.out.println(circuito.getComponente(posicion));
+                        opcion2 = c.identificar(circuito.getComponente(posicion));
                         switch(opcion2){
                             case 1:
-                                System.out.println("Ingrese el nuevo valor");
-                                nuevovalor = sc.nextFloat();
-                                componentes.get(posicion).setVoltaje(nuevovalor);
+                                System.out.println("Ingrese el nuevo valor de resistencia");
+                                nuevovalor = sc.nextInt();
+                                circuito.getComponente(posicion).setResistencia((int)nuevovalor);
                                 break;
                             case 2:
-                                System.out.println("Ingrese el nuevo valor");
+                                System.out.println("Ingrese el nuevo valor de capacitancia");
                                 nuevovalor = sc.nextFloat();
-                                componentes.get(posicion).setResistencia((int)nuevovalor);
+                                circuito.getComponente(posicion).setCapacitancia((int)nuevovalor);
                                 break;
                             case 3:
-                                System.out.println("Ingrese el nuevo valor");
+                                System.out.println("Ingrese el nuevo valor logico del switch: true o false");
+                                circuito.getComponente(posicion).setEstado(sc.nextBoolean());
+                                break;
+                            case 4:
+                                System.out.println("Ingrese el nuevo valor del voltaje");
                                 nuevovalor = sc.nextFloat();
-                                componentes.get(posicion).setCorriente(nuevovalor);
+                                circuito.getComponente(posicion).setVoltaje(nuevovalor);
+                                c.actualizar(circuito.getComponente(posicion).getVoltaje(), circuito);
+                                break;
+                            case 5:
+                                System.out.println("Es un Diodo nada mas sirve para rectificar no le puedes hacer cambios");
+                                break;
+                            case 6:
+                                System.out.println("Ingrese el nuevo valor del transistor");
+                                nuevovalor = sc.nextFloat();
+                                circuito.getComponente(posicion).setFrecuencia(nuevovalor);
                                 break;
                         }
                         break;
+                case 3:
+                    estado = false;
+                    break;
             }
-            System.out.println(componentes);
+            System.out.println(circuito);
+        }
+    }
+
+    public void actualizar(float voltaje, Circuito circuito){
+        for(int i = 0; i <= circuito.lenght()-1; i++){
+            circuito.getComponente(i).setVoltaje(voltaje);
         }
     }
 }
